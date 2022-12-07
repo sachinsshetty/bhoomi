5a93c9b2cc10dee888589b47a3f862c3a145cfbc
sahanashetty31 <30534288+sahanashetty31@users.noreply.github.com>
2022-12-07 11:37:31 +0530

    add mdtocsv code

--- src/food/src/com/diet/MdtoCsv.java
+++ src/food/src/com/diet/MdtoCsv.java
@@ -8,7 +8,7 @@ public class MdtoCsv
     public static void main(String[] args) throws Exception
     {
         // pass the path to the file as a parameter
-        File file = new File("C:\\Users\\Sachin\\Desktop\\2021-06-18-muskmelon-and-banana-smoothie.md");
+        File file = new File("E:\\Sahana\\github\\ammanaaduge.com\\_posts\\breakfast\\2021-10-30-mangalore-buns.md");
         Scanner sc = new Scanner(file);
 
         while (sc.hasNextLine())
