7e5f3a02f5f3d1a9702cb48fbe9cb70adfbcdeec
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2023-05-18 05:07:31 +0200

    Init Bhoomi backend

---
+++ src/bhoomi/bhoomi/src/test/kotlin/com/slabstech/bhoomi/BhoomiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.slabstech.bhoomi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BhoomiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
