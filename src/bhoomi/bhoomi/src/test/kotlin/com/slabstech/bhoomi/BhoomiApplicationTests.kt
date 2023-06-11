397d18e57e8a460cc3b9380f6979f0a78b93b642
Sachin Shetty <26170834+sachinsshetty@users.noreply.github.com>
2023-06-11 12:57:48 +0200

    feature/sseo poster v1 (#61)
    * Update Hompage info
    
    * Literature - Paper 1
    
    * Init Bhoomi backend
    
    * Add sample tests

---
+++ src/bhoomi/bhoomi/src/test/kotlin/com/slabstech/bhoomi/BhoomiApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.slabstech.bhoomi
+
+import com.slabstech.bhoomi.prep.ManipulateStrings
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BhoomiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
